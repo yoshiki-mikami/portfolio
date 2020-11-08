825e27043bd7c75d09682d418af587826a3943b8
dhr1325 <dhrworks1325@gmail.com>
2020-11-08 12:43:15 +0900

    a

---
+++ ash.js
@@ -0,0 +1,24 @@
+let youtuber = {
+    list:{
+        businesYoutuber:{
+            haisinsya:{
+                shimabu:{
+
+                },
+                attyann:{
+
+                }
+            },
+            manabi=function(){
+            }
+        },
+        entameYoutuber:{
+
+        }
+    },
+    kikakuUp: function(){
+        document.getElementById('ash').style.color = red;
+    }
+};
+
+export default youtuber;
